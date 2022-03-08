ffcd7c5e97657240859a697f712f68a6cbdf5a17
RamiRifaat <rrifaat@eastsideprep.org>
2022-03-08 14:41:00 -0800

    working final w/o perfect collision

--- js/game.js
+++ js/game.js
@@ -4,72 +4,84 @@ let keydownOutput = document.getElementById("keydown-output");
 let keyupOutput = document.getElementById("keyup-output");
 let player1param = document.getElementById("player1-param");
 let player2param = document.getElementById("player2-param");
-let player1X = 250;
-let player1Y = 250;
-let player2X = 250;
-let player2Y = 250;
-let player1Speed = 2;
-let player2Speed = 2;
+const GRAVITY = 1;
+const UP_VELOCITY = -10;
+const PLAYER_WIDTH = 30;
+const PLAYER_HEIGHT = 30;
+const ARENA_HEIGHT = 500;
+const ARENA_WIDTH = 500;
+
+const PLAYER1_LEFT = 74;
+const PLAYER1_RIGHT = 76;
+const PLAYER1_UP = 73;
+const PLAYER1_DOWN = 75;
+
+const PLAYER2_LEFT = 65;
+const PLAYER2_RIGHT = 68;
+const PLAYER2_UP = 87;
+const PLAYER2_DOWN = 83;
+
+const STARTING_SIDE_DISTANCE = 50;
+const STARTING_HEIGHT = 250;
+
+let player1X = 400;
+let player1Y = STARTING_HEIGHT;
+let player2X = 100;
+let player2Y = STARTING_HEIGHT;
+let player1XSpeed = 4;
+let player2XSpeed = 4;
+let player1YSpeed = 0;
+let player2YSpeed = 0;
 let player1XDir = 0;
 let player1YDir = 0;
 let player2XDir = 0;
 let player2YDir = 0;
-const PLAYER_WIDTH = 100;
-const PLAYER_HEIGHT = 20;
-const ARENA_HEIGHT = 500;
-const ARENA_WIDTH = 500;
+let player1Lives = 5;
+let player2Lives = 5;
+let health1meter = document.getElementById("health1Meter");
+
+health1meter.value = player1Lives;
+
+let health2meter = document.getElementById("health2Meter");
+
+health2meter.value = player2Lives;
 
 function drawPlayer1() {
+    ctx.fillStyle = '#FFA500';
     ctx.fillRect(player1X, player1Y, PLAYER_WIDTH, PLAYER_HEIGHT)
 }
 
 function drawPlayer2() {
+    ctx.fillStyle = '#3370d4';
     ctx.fillRect(player2X, player2Y, PLAYER_WIDTH, PLAYER_HEIGHT)
 }
 
 function movePlayer() {
     player1param.innerHTML = "movePlayer:" + player1X + " " + player1Y + " " + player1XDir + " " + player1YDir;
-    player1X += player1Speed * player1XDir;
-    player1Y += player1Speed * player1YDir;
+    player1X += player1XSpeed * player1XDir;
+
+    player1YSpeed += GRAVITY;
+
+    player1Y = player1Y + player1YSpeed;
     if (player1X < 0) {
         player1X = 0;
     } else if (player1X > ARENA_WIDTH - PLAYER_WIDTH) {
         player1X = ARENA_WIDTH - PLAYER_WIDTH;
     }
     if (player1Y < 0) {
-        player1Y = 0;
+        player1Y = 450;
     } else if (player1Y > ARENA_HEIGHT - PLAYER_HEIGHT) {
         player1Y = ARENA_HEIGHT - PLAYER_HEIGHT;
     }
 
 }
-/*
-function drawTriangle() {
 
-    ctx.beginPath();
-    ctx.moveTo(75, 50);
-    ctx.lineTo(100, 75);
-    ctx.lineTo(100, 25);
-    ctx.fill();
-
-}
-
-function drawTriangle2() {
-
-    ctx.beginPath();
-    ctx.moveTo(75, 50);
-    ctx.lineTo(100, 75);
-    ctx.lineTo(100, 25);
-    ctx.fill();
-
-}
-
-*/
 
 function movePlayer2() {
     player2param.innerHTML = "movePlayer2:" + player2X + " " + player2Y + " " + player2XDir + " " + player2YDir;
-    player2X += player2Speed * player2XDir;
-    player2Y += player2Speed * player2YDir;
+    player2X += player2XSpeed * player2XDir;
+    player2YSpeed += GRAVITY;
+    player2Y = player2Y + player2YSpeed;
     if (player2X < 0) {
         player2X = 0;
     } else if (player2X > ARENA_WIDTH - PLAYER_WIDTH) {
@@ -77,20 +89,13 @@ function movePlayer2() {
     }
 
     if (player2Y < 0) {
-        player2Y = 0;
+        player2Y = 450;
     } else if (player2Y > ARENA_HEIGHT - PLAYER_HEIGHT) {
         player2Y = ARENA_HEIGHT - PLAYER_HEIGHT;
     }
 }
-const PLAYER1_LEFT = 37;
-const PLAYER1_RIGHT = 39;
-const PLAYER1_UP = 38;
-const PLAYER1_DOWN = 40;
 
-const PLAYER2_LEFT = 65;
-const PLAYER2_RIGHT = 68;
-const PLAYER2_UP = 87;
-const PLAYER2_DOWN = 83;
+
 
 function keyPressed(event) {
     let key = event.keyCode;
@@ -99,26 +104,26 @@ function keyPressed(event) {
 
     }
     if (key === PLAYER1_RIGHT) {
-        player1XDir = +1;
+        player1XDir = 1;
     }
     if (key === PLAYER1_DOWN) {
-        player1YDir = +1;
+        //player1YDir = +1;
     }
     if (key === PLAYER1_UP) {
-        player1YDir = -1;
+        //player1YDir = -1;
     }
     if (key === PLAYER2_LEFT) {
         player2XDir = -1;
 
     }
     if (key === PLAYER2_RIGHT) {
-        player2XDir = +1;
+        player2XDir = 1;
     }
     if (key === PLAYER2_DOWN) {
-        player2YDir = +1;
+        //player2YDir = +1;
     }
     if (key === PLAYER2_UP) {
-        player2YDir = -1;
+        //player2YDir = -1;
     }
     keydownOutput.innerHTML = "Key down code:" + key + " " + player1XDir + " " + player1YDir + " " + player2XDir + " " + player2YDir;
 }
@@ -132,7 +137,9 @@ function keyReleased(event) {
         player1XDir = 0;
     }
     if (key === PLAYER1_UP) {
-        player1YDir = 0;
+        //player1YDir = 0;
+        // player1Y = player1Y - 15;
+        player1YSpeed = UP_VELOCITY;
     }
     if (key === PLAYER1_DOWN) {
         player1YDir = 0;
@@ -144,10 +151,11 @@ function keyReleased(event) {
         player2XDir = 0;
     }
     if (key === PLAYER2_DOWN) {
-        player2YDir = 0;
+        //player2YDir = 0;
     }
     if (key === PLAYER2_UP) {
-        player2YDir = 0;
+        //player2YDir = 0;
+        player2YSpeed = UP_VELOCITY;
     }
     keyupOutput.innerHTML = "Key up code:" + key + " " + player1XDir + " " + player1YDir + " " + player2XDir + " " + player2YDir;
 }
@@ -158,30 +166,94 @@ function checkImpact() {
         (player2Y < player1Y + PLAYER_HEIGHT) &&
         (player2Y + PLAYER_HEIGHT > player1Y)) {
         if (player1Y < player2Y) {
-            player2Y = ARENA_HEIGHT - PLAYER_HEIGHT;
+
+            updatelives(1);
+
         } else {
-            player1Y = ARENA_HEIGHT - PLAYER_HEIGHT;
+
+            updatelives(2);
         }
     }
 
+
+}
+
+function updatelives(playerhit) {
+
+    if (playerhit == 1) {
+        alert("Player 1 has been hit");
+        player1Lives = player1Lives - 1;
+        let health1meter = document.getElementById("health1Meter");
+        health1meter.value = player1Lives;
+
+
+    }
+    if (playerhit == 2) {
+        alert("Player 2 has been hit");
+        player2Lives = player2Lives - 1;
+        let health2meter = document.getElementById("health2Meter");
+        health2meter.value = player2Lives;
+    }
+    player1X = 400;
+    player1Y = 500;
+    player2X = 100;
+    player2Y = 500;
+    player1XDir = 0;
+    player1YDir = 0;
+    player2XDir = 0;
+    player2YDir = 0;
+
+
+    if (player1Lives == 0) {
+        endGame(1);
+
+    }
+    if (player2Lives == 0) {
+        endGame(2);
+
+    }
+
+}
+
+
+function endGame(losingplayer) {
+
+    alert("Player " + losingplayer + " has won");
+
+    player1Lives = 5;
+    player2Lives = 5;
+    health1meter = document.getElementById("health1Meter");
+    health1meter.value = player1Lives;
+    health2meter = document.getElementById("health2Meter");
+    health2meter.value = player2Lives;
+
+    alert("Restarting Game")
+
+
+}
+
+function gravity() {
+    // player1Y = player1Y + 0.15;
+    // player2Y = player2Y + 0.15;
 }
 
 function refreshUI() {
     ctx.clearRect(0, 0, 500, 500);
 
 
-    drawPlayer1();
-    drawPlayer2();
-    // drawTriangle();
-    // drawTriangle2();
+
     movePlayer();
     movePlayer2();
 
+    drawPlayer1();
+    drawPlayer2();
     checkImpact();
+
+
 
 }
 
 
 
-
+// setInterval(gravity, 1.5)
 setInterval(refreshUI, 30)
